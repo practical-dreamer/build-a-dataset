88e393e430adab325e7fbdd10a7544dc0eef2c6e
practicaldreamer <78515588+practicaldreamer@users.noreply.github.com>
2023-06-28 09:06:19 -0500

    restructuring directories

--- src/build-a-dataset2.ps1
+++
@@ -1,6 +0,0 @@
-python promptIt.py  --json jokes    --template openAI_chat`
-    --user_prompt   "Tell a knock knock joke"`
-    --bot_prompt    "Knock Knock"
-python promptIt.py  --json jokes`
-	--user_prompt 	"Who's There?"`
-	--bot_prompt 	""
